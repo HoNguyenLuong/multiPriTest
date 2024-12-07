09dc615dc7c92eb8b710cc02b7d96d2bcd197c12
Ho Nguyen Luong <hnluong.work02@gmail.com>
2024-12-07 12:17:32 +0700

    Add files via upload

---
+++ MainApp/radish.cpp
@@ -0,0 +1,12 @@
+#include "radish.h"
+#include <iostream>
+
+void Radish::printRadish(){
+    std::cout << "Radish" << std::endl;;
+}
+
+int Radish::numRadish(){
+    int a = 1;
+    if(a == 1) return 1;
+    return 0;
+}
---
+++ MainApp/radish.h
@@ -0,0 +1,10 @@
+#ifndef RADISH_H
+#define RADISH_H
+
+#endif // RADISH_H
+
+class Radish {
+public:
+    void printRadish();
+    int numRadish();
+};
